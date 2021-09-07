5634f542f53aed521ab7b8b70353f13f68613d9e
kimseokjin <kimseokjin0324@gmail.com>
2021-09-08 00:08:19 +0900

    상품 등록 처리-@ModelAttribute에 대한 다양한 실제 상품 등록 처리하는 방법에 대해서 2021/09/07

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -45,9 +42,45 @@ public class BasicItemController {
     public String addForm(){
         return "basic/addForm";
     }
+    //@PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model)
+    {
+        Item item=new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item",item);
+
+        return "basic/item";
+    }
+//    @PostMapping("/add")
+//    public String addItemV2(@ModelAttribute("item") Item item)
+//    {
+////        Item item=new Item();
+////        item.setItemName(itemName);
+////        item.setPrice(price);
+////        item.setQuantity(quantity);
+//        //@ModelAttribute가 만들어줌
+//
+//        itemRepository.save(item);
+//
+//        //model.addAttribute("item",item); 이거를 주석 처리해도됨
+//        //@ModelAttribute가 모델(뷰)에 직접 넣어주는 역할(자동추가)도 함
+//        return "basic/item";
+//    }
+
     @PostMapping("/add")
-    public String save(){
-        return "basic/addForm";
+    public String addItemV4(Item item)
+    {
+        //Item->item이라는 이름으로 모델에 담김
+        itemRepository.save(item);
+        return "basic/item";
     }
 
     /**
